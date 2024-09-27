5c75894e6f783d7019e70b12e51e1cfc96ea9958
kkoryaka <konstantin.koryaka@state.ca.gov>
2024-09-26 17:13:29 -0700

    updated mobile menu in the header

--- src/header/hamburger.js
+++ src/header/hamburger.js
@@ -1,9 +1,1 @@
-// hamburger menu
-window.addEventListener("load", () => {
-  const menuBtn = document.querySelector(".main-navigation-button");
-
-  menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("open");
-  });
-});
 
