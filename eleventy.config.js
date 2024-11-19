7b11cb12737fad48f6f9b1cfcaf703970f34b8ab
Jon Jensen <github@id.xjensen.com>
2024-11-19 09:53:45 -0800

    refactor: Simplify 11ty config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,3 @@
-import path from "node:path";
 import cssBuilder from "./tools/bundlers/cssBuilder.js";
 import jsBuilder from "./tools/bundlers/jsBuilder.js";
 
@@ -7,32 +6,13 @@ const cssBuildPath = "_site/css/bundle.css";
 const jsBuildPath = "_site/js/bundle.js";
 
 export default async function (eleventyConfig) {
-	eleventyConfig.addTransform(
-		"staticPaths",
-		/**
-		 * @param {string} content
-		 * @param {string} outputPath
-		 */
-		async (content, outputPath) => {
-			const basePath = "_site"; // Adjust this if your output directory is different
-			const relativePath = path
-				.relative(path.dirname(outputPath), path.dirname(basePath))
-				.slice(0, -2); // Removing last 2 dots
-			return content
-				.replace(/href="(.*\/)"/g, 'href="$1index.html"') // Fixing any root path links
-				.replace(/=\"\//g, `="${relativePath}`); // Replace all ... ="/  ... with new path
-		},
-	);
-
 	eleventyConfig.on("eleventy.before", async ({ runMode }) => {
-		const buildPromises = [];
 		// Only build all of the bundle files during first run, not on every change.
 		if (firstBuild || runMode !== "serve") {
-			buildPromises.push(cssBuilder(cssBuildPath));
-			buildPromises.push(jsBuilder(jsBuildPath));
+			await cssBuilder(cssBuildPath);
+			await jsBuilder(jsBuildPath);
 			firstBuild = false;
 		}
-		await Promise.all(buildPromises);
 	});
 
 	eleventyConfig.on("eleventy.beforeWatch", async (changedFiles) => {
@@ -54,7 +34,6 @@ export default async function (eleventyConfig) {
 		"tools/11ty/root": "/",
 	});
 
-	// Watching css updates
 	eleventyConfig.addWatchTarget("./src");
 	eleventyConfig.addWatchTarget("./pages");
 	eleventyConfig.addWatchTarget("./tools/11ty");
