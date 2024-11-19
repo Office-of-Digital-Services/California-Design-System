118aa697f800626251ebf683b34d7893d3aca001
Jon Jensen <github@id.xjensen.com>
2024-11-19 09:55:41 -0800

    refactor: Update homepage for new file structure

--- tools/11ty/index.11tydata.js
+++ tools/11ty/index.11tydata.js
@@ -2,39 +2,25 @@ import path from "node:path";
 import { glob } from "glob";
 
 export default async () => {
-	const partGlobs = await glob("src/**/*.@(njk|html)");
-	const parts = partGlobs
-		.map((part) => {
-			const parsedPath = path.parse(part);
-
-			const href = parsedPath.dir.endsWith(parsedPath.name)
-				? `/${parsedPath.dir}`
-				: `/${parsedPath.dir}/${parsedPath.name}`;
-			const group = path.basename(parsedPath.dir);
+	const pageGlobs = await glob("pages/**/*.@(njk|html|md)");
+	const pages = pageGlobs
+		.map((page) => {
+			const parsedPath = path.parse(page);
+			const href = `/${parsedPath.dir}/${parsedPath.name}`;
 			const name = parsedPath.name;
 
 			return {
 				href,
-				group,
 				name,
 			};
 		})
-		.sort((part) => part.group);
-
-	const pageGlobs = await glob("pages/**/*.@(njk|html)");
-	const pages = pageGlobs.map((page) => {
-		const parsedPath = path.parse(page);
-		const href = `/${parsedPath.dir}/${parsedPath.name}`;
-		const name = parsedPath.name;
-
-		return {
-			href,
-			name,
-		};
-	});
+		.sort((a, b) => {
+			if (a.name < b.name) return -1;
+			if (a.name > b.name) return 1;
+			return 0;
+		});
 
 	return {
-		parts,
 		pages,
 	};
 };
