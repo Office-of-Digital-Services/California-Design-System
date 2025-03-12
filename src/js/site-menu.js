18a0e1dd16c4671a69d2bdcd8ed05218ea78f646
Jon Jensen <github@id.xjensen.com>
2025-03-12 15:10:08 -0700

    chore: burger trigger

--- src/js/site-menu.js
+++ src/js/site-menu.js
@@ -6,11 +6,11 @@ class SiteMenu extends HTMLElement {
     const width = window.innerWidth;
 
     /* Some browsers need a tick to see current DOM contents of the custom element. */
-    /* Without timeout, we'll append instead of replace. */
     window.setTimeout(() => {
       /* First, convert the burger link into a burger button. */
       const link = this.querySelector("a");
       const button = document.createElement("button");
+			button.setAttribute("aria-expanded", "false");
       button.innerHTML = link.innerHTML;
       link.replaceWith(button);
 
@@ -24,6 +24,17 @@ class SiteMenu extends HTMLElement {
       });
 
       observer.observe(header);
+
+			button.addEventListener("click", (event) => {
+				const expanded = button.getAttribute("aria-expanded");
+				const wasExpanded = (expanded === "true");
+
+				const buttonIcon = button.querySelector("ca-icon");
+
+				this.toggleAttribute("expanded");
+				buttonIcon.setAttribute("name", (wasExpanded ? "bear-menu" : "close"));
+				button.setAttribute("aria-expanded", (wasExpanded ? "false" : "true"));
+			});
     }, 1);
   }
 }
